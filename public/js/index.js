60bdbfd5d4fe4f561285a638abb419a4134cbd09
doej96 <doej96@naver.com>
2021-01-04 13:06:34 +0900

    ...

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+// opneweathermap: 488c40db3b8e389e1bcf4a0f9a83f8fa
+// kakao: 8f8ecf68fb72a64ab9800fd79fb08d2a
+
+// 7days: https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=488c40db3b8e389e1bcf4a0f9a83f8fa&units=metric&exclude=minutely,hourly
